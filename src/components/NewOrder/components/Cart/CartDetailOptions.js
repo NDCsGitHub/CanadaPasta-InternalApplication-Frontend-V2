f4e4ef3199fe939758d8123d0c1b1bc85f4b1518
NDC <andy_chen123@hotmail.com>
2022-05-10 17:16:56 -0400

    ss

--- src/components/NewOrder/components/Cart/CartDetailOptions.js
+++ src/components/NewOrder/components/Cart/CartDetailOptions.js
@@ -18,13 +18,12 @@ export default function CartDetailOptions() {
   return (
 
     <div className='cartDetailContainer'>
-
         <Stack spacing={2}>
             <Item>Item 1</Item>
-            <Item>Item 2</Item>
-            <Item>Item 3</Item>
+            
         </Stack>
-        
     </div>
   )
 }
+
+
