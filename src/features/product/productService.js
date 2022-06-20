b354d6d834630d459b7af4b072250d55c588f010
NDC <79379545+NDCsGitHub@users.noreply.github.com>
2022-06-19 22:56:35 -0400

    adding get all product serivce call

--- src/features/product/productService.js
+++ src/features/product/productService.js
@@ -6,3 +6,24 @@ import axios from 'axios'
 const API_URL = '/api/products'
 
 
+// get all products
+const getAllProducts = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+
+        }
+    }
+
+    const response = await axios.get(API_URL, config)
+
+    return response.data
+}
+
+
+const productService = {
+    getAllProducts,
+
+}
+
+export default productService
